1cbf52465fefe93b010dac09fbb37b5e1607d084
Rodrigo Monteiro de Moraes de Arruda Falcao <rmmaf@ug3c21.windows.cin.ufpe.br>
2017-09-16 15:34:10 -0300

    creio que vai

--- min.cpp
+++ min.cpp
@@ -21,7 +21,7 @@ struct compare{
 
     }
 };
-bool equalsArrayBi (double array[2], double array1[2]){
+static bool equalsArrayBi (double array[2], double array1[2]){
 	bool final = true;
 	for (int i = 0; i < 2; ++i)
 	{
@@ -42,23 +42,28 @@ int main(){
 	double coordFinal[2];
 	double coordTemp[2];
 	ponto atual = ponto( distRet(coordFinal, coordAtual) , coordAtual);
-	while(!compareArrayBi(coordAtual, coordFinal)){//testa se o ponto atual é igual ao final
+	ponto anterior = atual;
+
+	stack< priority_queue<ponto, vector<ponto>, compare > > pilha;
+	priority_queue<ponto, vector<ponto>, compare >pq;
+
+	while(!equalsArrayBi(coordAtual, coordFinal)){//testa se o ponto atual é igual ao final
 		//Fase de exploracao
-		priority_queue<Node, vector<Node>, compare >pq;
-		if(mapa[coordAtual[0] - 51][coordAtual[1]] != '#'){
-			coordTemp[0] = coordAtual[0] - 51;//andar para a esquerda
+
+		if(mapa[coordAtual[0] - 51][coordAtual[1]] == ' '){
+			coordTemp[0] = coordAtual[0] - 51;//andar para esquerda
 			coordTemp[1] = coordAtual[1];
 			pq.push(ponto( distRet(coordTemp, coordFinal) , coordTemp));
-		} else if(mapa[coordAtual[0] + 51][coordAtual[1]] != '#'){
-			coordTemp[0] = coordAtual[0] + 51;//andar para a direita
+		} else if(mapa[coordAtual[0] + 51][coordAtual[1]] == ' '){
+			coordTemp[0] = coordAtual[0] + 51;//andar para direita
 			coordTemp[1] = coordAtual[1];
 			pq.push(ponto( distRet(coordTemp, coordFinal) , coordTemp));
-		} else if (mapa[coordAtual[0]][coordAtual[1] + 51] != '#')
+		} else if (mapa[coordAtual[0]][coordAtual[1] + 51] == ' ')
 		{
 			coordTemp[0] = coordAtual[0];//andar para cima
 			coordTemp[1] = coordAtual[1] + 51;
 			pq.push(ponto( distRet(coordTemp, coordFinal) , coordTemp));
-		} else if (mapa[coordAtual[0]][coordAtual[1] - 51] != '#')
+		} else if (mapa[coordAtual[0]][coordAtual[1] - 51] == ' ')
 		{
 			coordTemp[0] = coordAtual[0];//andar para baixo
 			coordTemp[1] = coordAtual[1] - 51;
@@ -66,16 +71,25 @@ int main(){
 		}
 
 		//fase de comparacao (ver se o topo da heap e melhor)
-		ponto topo = pq.top();
-		pq.pop();//tirar ponto atual da pq, pois caso ele seja ruim explora o "segundo ponto melhor na proxima" 
+		ponto topo = pq.top(); 
 		if(topo.heuristica > atual.heuristica){//ponto pior
-			for (int i = 0; i < 4; ++i)
-			{
-				topoTemp = pq.top();
-				mapa[topoTemp.coordenadas[0]][topoTemp.coordenadas[1]] = '#';
+			mapa[atual.coordenadas[0]][atual.coordenadas[1]] = '*';//ponto atual nao é um ponto bom
+			for(int i = 0; i < 4; i++){
+				if(!equalsArrayBi(anterior.coordenadas, topo.coordenadas)){
+					mapa[topo.coordenadas[0]][topo.coordenadas[1]] = '*';
+				}
 				pq.pop();
+				topo = pq.top();
 			}
+			pq = pilha.top();
+			atual = anterior;
+			pilha.pop();
+		} else{
+			anterior = atual;//o ponto anterior será o que era atual
+			atual = topo;//o atual sera o melhor
+			pilha.push(pq);//e essa pq vai para pilha
+			pq = priority_queue<ponto, vector<ponto>, compare >();
 		}
-
 	}
+	return 0;
 }
